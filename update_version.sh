d9e4501d2fc523a7bc461dce9e0389118cc62211
Martin Schiller <waotzi@proton.me>
2023-12-03 09:55:10 +0700

    Update InnerBreeze to version 1.1.2

--- update_version.sh
+++ update_version.sh
@@ -1,27 +1,14 @@
 #!/bin/bash
 
-# Define the repository
 REPO="naoxio/inner_breeze"
-
-# Get the latest tag from GitHub
 LATEST_TAG=$(curl -s "https://api.github.com/repos/$REPO/releases/latest" | jq -r .tag_name)
 echo "Latest tag: $LATEST_TAG"
-
-# Define the URL of the tar.gz file
 TAR_URL="https://github.com/$REPO/releases/download/$LATEST_TAG/InnerBreeze-Linux-x86_64.tar.gz"
 echo "Tar URL: $TAR_URL"
-
-# Download the tar.gz file
 curl -sL $TAR_URL -o temp.tar.gz
-
-# Calculate the sha256 hash of the tar.gz file
 SHA256_HASH=$(sha256sum temp.tar.gz | awk '{ print $1 }')
 echo "SHA256 Hash: $SHA256_HASH"
-
-# Remove the downloaded tar.gz file
 rm temp.tar.gz
-
-# Update the JSON file
 JSON_FILE="io.naox.InnerBreeze.json"
 jq --arg url "$TAR_URL" --arg hash "$SHA256_HASH" \
    '(.modules[] | select(.name == "InnerBreeze") | .sources[] | select(.type == "archive")).url = $url |
@@ -30,3 +17,8 @@ jq --arg url "$TAR_URL" --arg hash "$SHA256_HASH" \
 
 echo "JSON file updated."
 
+git add $JSON_FILE
+git commit -m "Update InnerBreeze to version $LATEST_TAG"
+git push
+
+echo "Git commit and push done."
